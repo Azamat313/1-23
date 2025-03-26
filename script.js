3789f4f7eec9c4c44ed4d298b05bc6909d5a2a26
fukiniz <danamarat648@gmail.com>
2025-03-26 12:46:59 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,66 @@
-function checkPassword() {
-    const password = document.getElementById("password").value;
-    const status = document.getElementById("status");
+"use strict"
 
-    if (password.length >= 8) {
-        status.innerHTML = "✔️ Password is strong!";
-        status.style.color = "green";
-    } else {
-        status.innerHTML = "❌ Password is too short!";
-        status.style.color = "red";
-    }
+let discount = 0;
 
-    // Add animation
-    status.style.opacity = "1";
-    status.style.transform = "scale(1.1)";
-    
-    // Reset size after 300ms
-    setTimeout(() => {
-        status.style.transform = "scale(1)";
-    }, 300);
+const discountValueElement = 
+document.getElementById('discountValue');
+const finalAmountElement =
+document.getElementById('finalAmouunt');
+const purchaseButton =
+document.getElementById('purchaseButton');
+
+purchaseButton.addEventListener('click', () => {
+
+while (discount < 10) {
+    discount = discount + 1;
+    console.log(discount); 
+    document.getElementById('discountValue').innerText = 'Current Discount:' + discount;
+    break;
 }
 
-document.getElementById("password").addEventListener("input", checkPassword);
+
+})
+
+
+"use sctrict";
+
+let discount1 = 0;
+
+while (discount1 < 10) {
+    discount1 = discount1 + 1;
+    console.log(discount1); 
+}
+
+
+
+let num = 2;
+
+for (let i = 1;i <= 10; i++){
+    console.log(num * i);
+
+
+}
+
+
+let numb = 2;
+
+let i = 1;
+
+while(true) {
+    console.log(numb * i)
+    i++;
+    if(i > 100)break;
+}
+
+
+
+
+let count = 0;
+
+for (let i = 1; i <= 20; i++){
+    if (i % 2 === 0) {
+        count++;
+    }
+}
+
+console.log(count);
